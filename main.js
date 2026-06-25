70b3ddeff731ab7539ff12cc8fd4f4fcbc64d77b
Nuriddin-Baratov <baratovnuriddin7@gmail.com>
2026-06-25 16:25:06 +0500

    s

---
+++ main.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+
+  loader.classList.add(
+    "opacity-0",
+    "transition-opacity",
+    "duration-700"
+  );
+
+  setTimeout(() => {
+    loader.remove();
+  }, 700);
+});
